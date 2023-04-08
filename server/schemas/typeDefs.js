eba8e436f96e07e8a0b606a0e291574626b633a0
Drew S <andrewsantos2022@u.northwestern.edu>
2023-04-07 21:56:13 -0500

    deployment

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,53 +1,47 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type User {
-        _id: ID
-        username: String!
-        email: String!
-        bookCount: Int
-        savedBooks: [Book]
-    }
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    type Book {
-        _id: ID
-        bookId: String!
-        authors: [String]
-        description: String!
-        title: String!
-        image: String
-        link: String
-    }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Query {
-        me: User
-    }
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookData: BookInput!): User
-        removeBook(bookId: ID!): User
-    }
+  type Query {
+    me: User
+  }
 
-    input BookInput {
-        bookId: String!
-        authors: [String]
-        description: String!
-        title: String!
-        image: String
-        link: String
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
-
-
-
-
-
